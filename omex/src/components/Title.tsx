743b412bd5fe1cfd2305a42da9905dd8d8620822
Ruben Hernandez <Rubenhnt@gmail.com>
2025-08-28 16:57:43 +0000

    title css

--- omex/src/components/Title.tsx
+++ omex/src/components/Title.tsx
@@ -1,9 +1,21 @@
 export const Title = ({ text }: { text: string }) => {
+const style = {
+    background: 'repeating-linear-gradient(45deg, #fafafaff 0px, #90bad3ff 1px, transparent 1px, transparent 6px)'
+}
+const style2 = {
+    background: 'repeating-linear-gradient(45deg, #fafafaff 0px, #90bad3ff 1px, transparent 1px, transparent 3px)'
+}
     return (
         <div className="w-full py-4 grid grid-cols-6 gap-0">
-            <div className="col-span-1">1</div>
-            <h1 className="text-2xl font-bold mb-4 col-span-4">{text}</h1>
-            <div className="col-span-1">2</div>
+            <div className="col-span-1" style={style2}></div>
+            <h1
+                className="text-3xl text-center py-4 col-span-4 font-aleo font-bold tracking-wider"
+                style={style}
+            >
+                {text}
+            </h1>
+            <div className="col-span-1" style={style2}></div>
+
         </div>
     )
 }

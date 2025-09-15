3ee5bf352d80e736552e9728a1d67f95e07869a8
Ruben Hernandez <Rubenhnt@gmail.com>
2025-09-15 22:17:33 +0000

    header positioning

--- omex/src/components/Header.tsx
+++ omex/src/components/Header.tsx
@@ -4,11 +4,11 @@ import { Menu } from './Menu';
 export const Header = () => {
 	const [showMenu, setShowMenu] = useState(false);
 	return (
-		<header className="flex flex-row justify-between p-2 fixed top-0 right-0 left-0 p-2 z-20 bg-transparent">
-			<h1 className="text-sm font-bold">OMEX</h1>
+		<header className="flex flex-row justify-between p-2 fixed top-2 right-0 left-0 p-2 z-20 bg-transparent">
+			<h1 className="text-sm font-bold ml-2">OMEX</h1>
 			<div className="flex flex-col justify-end items-end gap-0 bg-transparent">
 				<button
-					className="hover:cursor-pointer transition"
+					className="hover:cursor-pointer transition mr-2"
 					onClick={() => setShowMenu(!showMenu)}
 				>
 					<svg

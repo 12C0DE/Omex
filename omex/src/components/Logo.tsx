db3ca4f1ed08a1845078d8935c1e1002d7d78129
Ruben Hernandez <Rubenhnt@gmail.com>
2025-09-25 01:32:36 +0000

    setting logo

--- omex/src/components/Logo.tsx
+++ omex/src/components/Logo.tsx
@@ -6,21 +6,17 @@ export const Logo = () => {
 		<div className="flex flex-col items-center justify-center mt-10">
 			{theme === 'light' ? (
 				<img
-					src="/images/omex_logo_solo_gradient_bg.svg"
+					src="/images/omex_logo_solo_gradient_bg_wText.svg"
 					alt="Omex Logo"
 					className="h-24 w-24 mt-10 md:h-32 md:w-32 lg:h-48 lg:w-48"
 				/>
 			) : (
 				<img
-					src="/images/omex_logo_solo.svg"
+					src="/images/omex_logo_solo_gradient_bg_wText_Dark.svg"
 					alt="Omex Logo"
 					className="h-24 w-24 mt-10 md:h-32 md:w-32 lg:h-48 lg:w-48"
 				/>
 			)}
-			<h1 className="text-4xl font-bold text-center mt-1 tracking-[8px]">
-				OMEX
-			</h1>
-			<h2 className="text-center -mt-1 text-lg tracking-wide">CONSTRUCTION</h2>
 		</div>
 	);
 };

66bb22b17d618f5331ec72acb821252aca8cf0ee
Ruben Hernandez <Rubenhnt@gmail.com>
2025-09-25 00:28:55 +0000

    updated mission statement

--- omex/src/pages/Home.tsx
+++ omex/src/pages/Home.tsx
@@ -18,12 +18,11 @@ export const Home = () => {
 		>
 			<ScreenContainer idName="home">
 				<Logo />
-				<div className="mt-8 max-w-lg">
-					<p className="px-2 md:px-0 md:line-clamp-3 font-kanit font-light">
-						Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet
-						consectetur adipiscing elit quisque faucibus ex. Adipiscing elit
-						quisque faucibus ex sapien vitae pellentesque.
-					</p>
+				<div className="mt-12 max-w-lg">
+					<h1 className="px-2 md:px-0 md:line-clamp-3 font-kanit text-2xl md:text-3xl lg:text-4xl text-center font-semibold text-gray-700">
+						Building your vision,
+					</h1>
+					<h1 className="px-2 md:px-0 md:line-clamp-3 font-kanit text-2xl md:text-3xl lg:text-4xl text-center font-semibold text-gray-700">one project at a time.</h1>
 				</div>
 			</ScreenContainer>
 			<Projects />

ac5db6c4f799212d808312fa9221ecb064cac93e
Ruben Hernandez <Rubenhnt@gmail.com>
2025-10-14 22:10:08 +0000

    update projectTypes to images

--- omex/src/types/ProjectTypes.ts
+++ omex/src/types/ProjectTypes.ts
@@ -1,7 +1,7 @@
 export type ProjectType = {
 	title: string;
 	description: string;
-	img?: string | string[];
+	images?: string | string[];
 	thumbnail?: string;
 	clicked?: () => ProjectType | void;
 };

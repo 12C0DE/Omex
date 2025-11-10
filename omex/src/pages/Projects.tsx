64f9489804ef00457543c6969cfb544b17fb0bad
Ruben Hernandez <Rubenhnt@gmail.com>
2025-11-10 23:01:24 +0000

    added x scrolling to projects

--- omex/src/pages/Projects.tsx
+++ omex/src/pages/Projects.tsx
@@ -33,18 +33,17 @@ export const Projects = () => {
 	return (
 		<ScreenContainer idName="projects">
 			<Title text="Projects" id="projects2" />
-			<div className="flex flex-row flex-wrap gap-1 w-full mx-0 lg:w-lg lg:mx-2 max-h-[70vh] overflow-y-auto justify-center">
-				{projects.map((project) => {
-					return (
+			<div className="flex flex-row flex-nowrap gap-4 lg:mx-2 items-center overflow-x-auto py-4">
+				{projects.map((project) => (
+					<div key={(project as any).id ?? project.title} className="flex-shrink-0">
 						<ProjectCard
-							key={uuidv4()}
 							title={project.title}
 							img={project.thumbnail}
 							description={project.description}
 							clicked={() => showProject(project)}
 						/>
-					);
-				})}
+					</div>
+				))}
 			</div>
 			{showModal ? (
 				<ProjectModal
